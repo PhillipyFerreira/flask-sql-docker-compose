63b2d118641660f94d33ac70eb0e0ccfe0b8d259
Richard Paul <richardchartrain@gmail.com>
2023-09-16 18:01:01 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ DEFAULT_APP_NAME = 'flaskapp'
 
 def create_app(package_name):
     """Flask app factory."""
-    app = Flask(package_name, instance_relative_config=False)
+    app = Flask(package_name, instance_relative_config=True)
 
     configure_app(app)
 
