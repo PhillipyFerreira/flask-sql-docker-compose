99acc60ae5ebe11b090a7c708e8484d4f679f4d0
diegolelisceub <145225748+diegolelisceub@users.noreply.github.com>
2023-09-16 18:41:28 -0300

    Update run_celery.py - Diego Lélis
    Renomeada a variável TaskBase para taskbase, para atender convenção de nomenclatura python:S117.

--- app/run_celery.py
+++ app/run_celery.py
@@ -11,14 +11,14 @@ def make_celery(app=None):
     app = app or create_app('celeryapp', os.path.dirname(__file__))
     celery = Celery(__name__, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
-    TaskBase = celery.Task
+    taskBase = celery.Task
 
-    class ContextTask(TaskBase):
+    class ContextTask(taskBase):
         abstract = True
 
         def __call__(self, *args, **kwargs):
             with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
+                return taskBase.__call__(self, *args, **kwargs)
 
     celery.Task = ContextTask
 
