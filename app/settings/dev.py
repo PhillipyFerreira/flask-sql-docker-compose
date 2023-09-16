838b30fc650ca1e89575d85096224a3c6280e619
toshiow <145226026+toshiow@users.noreply.github.com>
2023-09-16 16:09:30 -0300

    Update dev.py

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import os
+
+user = os.environ["PG_USER"]
+password = os.environ["PG_PASSWORD"]
 
 DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
@@ -8,7 +12,7 @@ PORT = 5000
 
 LOG_FOLDER = '/tmp/'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://flask:flask@postgresql/flask'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@postgresql/flask'
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
