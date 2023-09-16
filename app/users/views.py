c7cea416ca30b58e73bc553e0d91fe29724f99ed
Richard Paul <richardchartrain@gmail.com>
2023-09-16 18:17:01 -0300

    Update views.py

--- app/users/views.py
+++ app/users/views.py
@@ -24,10 +24,10 @@ except ImportError:
 ACTION_2 = 'users.login'
 
 
-users = Blueprint('users', __name__)
+methods = Blueprint('users', __name__)
 
 
-@users.route('/login', methods=['GET', 'POST'])
+@methods.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     form = LoginForm()
@@ -45,7 +45,7 @@ def login():
     return render_template('users/login.html', form=form, error=error)
 
 
-@users.route('/logout')
+@methods.route('/logout')
 @requires_login
 def logout():
     logout_user()
@@ -54,7 +54,7 @@ def logout():
     return redirect(url_for(ACTION_2))
 
 
-@users.route('/signup', methods=('GET', 'POST'))
+@methods.route('/signup', methods=('GET', 'POST'))
 def signup():
     form = SignUpForm()
     session.pop('client_id', None)
@@ -109,7 +109,7 @@ def signup():
     return render_template('users/signup.html', form=form)
 
 
-@users.route('/settings', methods=('GET', 'POST'))
+@methods.route('/settings', methods=('GET', 'POST'))
 @requires_login
 def settings():
     form = SettingsForm()
@@ -179,7 +179,7 @@ def populate_form(form, user):
     form.phone.data = user.phone
 
 
-@users.route('/confirm/<token>')
+@methods.route('/confirm/<token>')
 def confirm_email(token):
     try:
         ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
@@ -202,7 +202,7 @@ def confirm_email(token):
     return redirect(url_for(ACTION_2))
 
 
-@users.route('/reset', methods=["GET", "POST"])
+@methods.route('/reset', methods=["GET", "POST"])
 def reset():
     form = EmailForm()
     if request.method == 'POST' and form.validate_on_submit():
@@ -237,7 +237,7 @@ def reset():
     return render_template('users/reset.html', form=form)
 
 
-@users.route('/reset/<token>', methods=["GET", "POST"])
+@methods.route('/reset/<token>', methods=["GET", "POST"])
 def reset_with_token(token):
     try:
         ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
