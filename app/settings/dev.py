8dad4d78c4b7013b5620de27a896b104e6e88332
PedroCarvalho <pedro.eugenio@sempreceub.com>
2023-09-17 19:31:28 -0300

    update dev.py

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-
+import tempfile
+import os
 DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 SECRET_KEY = '<replace with a secret key>'
 HOST = '0.0.0.0'
 PORT = 5000
-
-LOG_FOLDER = '/tmp/'
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://flask:flask@postgresql/flask'
+#Escondendo senha e login do banco de dados conforme sonarcloud 
+LOG_FOLDER = tempfile.mkdtemp()
+user = os.environ["PG_USER"]    
+password = os.environ["PG_PASSWORD"]
+testedb= os.environ["POSTGRES_DB"]
+SQLALCHEMY_DATABASE_URI = f"postgres://{user}:{password}@postgresql/{testedb}"
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -21,9 +24,13 @@ CELERY_RESULT_PERSISTENT = True
 CELERYD_POOL_RESTARTS = True
 CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
 
+#Escondendo senha e login do e-mail
+username_m = os.getenv("username") # Compliant
+password_m = os.getenv("password") # Compliant
+usernamePassword = 'user=%s&password=%s' % (username, password) # Compliant{code}
 MAIL_SERVER = 'postfix'
 MAIL_PORT = 25
 MAIL_USE_TLS = False
-MAIL_USERNAME = 'postfix'
-MAIL_PASSWORD = 'postfix'
+MAIL_USERNAME = username_m
+MAIL_PASSWORD = password_m
 MAIL_DEFAULT_SENDER = 'support@postfix'
