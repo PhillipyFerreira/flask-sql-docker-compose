a8cfdcddc9769e95fcae2bde2f8073735de7531d
anaclarasempreceub <145225870+anaclarasempreceub@users.noreply.github.com>
2023-09-17 12:10:33 -0300

    Update views.py - Ana Clara Lourena
    Alteração em fonte onde os métodos HTTP estavam precisando de uma correção, já que o GET é passado no código mas não tinha uma condicional pra ele.

--- app/users/views.py
+++ app/users/views.py
@@ -28,11 +28,11 @@ except ImportError:
 users = Blueprint('users', __name__)
 
 
-@users.route('/login', methods=['GET', 'POST'])
+@users.route('/login', methods=['POST'])
 def login():
     error = None
     form = LoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    if form.validate_on_submit():
         user = db.session.query(User).get(form.user.id)
         if user.check_password(form.password.data):
             login_user(user)
@@ -175,10 +175,10 @@ def confirm_email(token):
     return redirect(url_for(usrlog))
 
 
-@users.route('/reset', methods=["GET", "POST"])
+@users.route('/reset', methods=["POST"])
 def reset():
     form = EmailForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first_or_404()
         logging.debug(
             "Password reset request from {0}".format(
@@ -210,7 +210,7 @@ def reset():
     return render_template('users/reset.html', form=form)
 
 
-@users.route('/reset/<token>', methods=["GET", "POST"])
+@users.route('/reset/<token>', methods=["POST"])
 def reset_with_token(token):
     try:
         ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
@@ -219,7 +219,7 @@ def reset_with_token(token):
         logging.error(e)
         abort(404)
     form = PasswordForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    if form.validate_on_submit():
         user = User.query.filter_by(email=email).first_or_404()
         user.set_password(form.password.data)
         db.session.add(user)
