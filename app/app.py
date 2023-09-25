67ed16a1d71fa2fe2cd1dadc1b44c796742094cc
Hailton Reis <hailtonreis@users.noreply.github.com>
2023-09-25 02:24:56 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -29,7 +29,7 @@ def create_app(package_name,
                settings_override=None,
                register_security_blueprint=True):
     """Flask app factory."""
-    app = Flask(package_name, instance_relative_config=False)
+    app = Flask(package_name, instance_relative_config=True)
 
     configure_app(app, settings_override)
 
