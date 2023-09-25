7b51a5b1fb669d65763405c9260f82f14aa4b009
Hailton Reis <hailtonreis@users.noreply.github.com>
2023-09-25 02:35:33 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -27,7 +27,7 @@ DEFAULT_APP_NAME = 'flaskapp'
 
 def create_app(package_name,
                settings_override=None,
-               register_security_blueprint=True):
+               ):
     """Flask app factory."""
     app = Flask(package_name, instance_relative_config=True)
 
@@ -44,7 +44,7 @@ def create_app(package_name,
     return app
 
 
-def configure_app(app, config=None):
+def configure_app(app):
     """Configure application."""
     app.config.from_object('settings.base')
     if not app.config['TESTING']:
